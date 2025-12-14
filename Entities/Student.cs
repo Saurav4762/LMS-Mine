b276e3a1dd2ef225d358e37d3e0d01694edd2bf1
Saurav4762 <upretysaurav4@gmail.com>
2025-12-14 19:15:42 +0545

    Feat : Entities updated

---
+++ Entities/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace LMS_project.Entities;
+
+public class Student
+{
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public string Email { get; set; }
+    
+    public string Phone { get; set; }
+    
+    public bool IsActive { get; set; }
+    
+    public ICollection<BookIssue> BookIssues { get; set; }
+    
+}
