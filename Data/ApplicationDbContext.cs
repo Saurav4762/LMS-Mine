781ad7fbb329a35fe2fa3cc930beee5ec9559fc6
Saurav4762 <upretysaurav4@gmail.com>
2025-12-15 08:16:57 +0545

    Feat : Data updated

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LMS_project.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace LMS_project.Data;
 
@@ -8,5 +9,16 @@ public class ApplicationDbContext : DbContext
     {
         
     }
+    public DbSet<Student> Students { get; set; }
+    
+    public DbSet<Author> Authors { get; set; }
+    
+    public DbSet<Book> Books { get; set; }
+    
+    public DbSet<Category> Categories { get; set; }
+    
+    public DbSet<BookIssue> BookIssues { get; set; }
+    
+    public DbSet<Fine>  Fines { get; set; } 
 
 }
